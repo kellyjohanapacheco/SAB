72aa4232caddcb7bbf26cfb2ceea6ec0af19af42
kellypacheco <kelly.pacheco@udea.edu.co>
2017-11-22 07:13:37 -0500

    prestamocontroller

---
+++ SAB/SAB/Datos/Bien.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SAB.Datos
+{
+    [Table ("Bienes")]
+    public class Bien
+    {
+
+        public Bien()
+        {
+            this.PrestamoActivo = new HashSet<PrestamoActivo>();
+        }
+
+
+
+
+        [Key]
+        public long IdBien { get; set; }
+        public string Nombre { get; set; }
+        public string NumeroSerial { get; set; }
+        public string Valor { get; set; }
+        public string Descripcion { get; set; }
+        public string NombrePersonaCargo { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+
+        public virtual ICollection<PrestamoActivo> PrestamoActivo { get; set; }
+
+    }
+}
---
+++ SAB/SAB/Datos/BienContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SAB.Datos
+{
+    public class BienContext: DbContext
+    {
+        public BienContext() : base("name=Bien")
+        {
+            Database.SetInitializer(new BienInicializador());
+        }
+
+        public DbSet<Empleado> Empleados { get; set;  }
+        public DbSet<Bien> Bien { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Bien>()
+                .HasMany(p => p.PrestamoActivo)
+                .WithRequired(p => p.Bien)
+                .HasForeignKey(p => p.IdBien)
+                .WillCascadeOnDelete(false);
+            
+            
+            
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
